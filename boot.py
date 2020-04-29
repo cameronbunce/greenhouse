bb2bbf1d0a0593b9bf9c01a51ecb5cc7f5410ad1
Cameron Bunce <cameronfbunce@gmail.com>
2020-04-29 12:03:07 -0400

    Changes to be committed: 	new file:   .gitignore 	modified:   boot.py
    Added secret-keeping to boot.py

--- boot.py
+++ boot.py
@@ -1,10 +1,14 @@
+#secrets.py as a super simple not-sharing-to-github secret manager
+#comment out below if you want to just inline your SSID information
+from secrets import SSID SSIDPass
+
 def connect():
     import network
     sta_if = network.WLAN(network.STA_IF)
     if not sta_if.isconnected():
         print('connecting to network...')
         sta_if.active(True)
-        sta_if.connect(SSID, password)
+        sta_if.connect(SSID, SSIDPass)
         while not sta_if.isconnected():
             pass
     print('network config:', sta_if.ifconfig())
